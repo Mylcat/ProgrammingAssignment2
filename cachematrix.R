c0b40d28051ecf23c27adcc8ed9f8b132783587d
unknown <604404945@BTG463600.iuser.iroot.adidom.com>
2014-08-24 21:32:00 +0800

    Updated both functions makeCacheMatrix and cacheSolve for checking the inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve are used to cache data 
+## while traversing through loops.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix takes the data of a matrix 
+## and defines 4 functions to set data ,get data, set Inverse 
+## and get Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inv) m <<- inv
+        getInverse <- function() m
+        
+        # list thats maintained for the caching        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
@@ -12,4 +26,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        data <- x$get()
+        if(!is.null(m) ) {
+                
+                # the check has been introduced to make sure the data 
+                # and the inverse are valid by multiplying them to check for 
+                # identity matrix of same dim
+                # m %*% data should give an identity matrix 
+                # identical function is used to check 2 matricies if they are identical
+                # identity matrix.diag(nrow(m)) will give us an identity matrix
+                if(nrow(m)>0 & identical(( m %*% data),diag(nrow(m)))){
+                        message("getting cached data")
+                        return(m)
+                }
+        }
+        
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
